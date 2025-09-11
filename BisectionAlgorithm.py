d4ffd542419ab6a1fd8276f765e26c751f7641aa
Gabriel Gacovicaj <gabriel.gacovicaj@gmail.com>
2025-09-11 10:54:21 -0500

    Added comments to the algorithm

--- BisectionAlgorithm.py
+++ BisectionAlgorithm.py
@@ -28,27 +28,42 @@ def BisectionAlgorithm(f, a, b, TOL, maxIters):
         - iters : int
         Number of iterations used.
     """
-
+    # calculating f(x) on the interval extremes 
     FA = f(a)
     FB = f(b)
+
 
     if (FA == 0):
         return FA, 0
     elif (FB == 0):
         return FB, 0
+    
+    # if the multiplication of the sign gives a positive result 
+    # it means that the extremes of the interval are on the same side 
+    # of the x axis 
     elif ((np.sign(FA) * np.sign(FB)) > 0):
          raise ValueError("f(a) and f(b) must have opposite signs.")
 
+
     i = 1
     while i < maxIters:
+        # calculate the middle point 
         p = a + (b - a)/2
+
+        # evaluate f(x) on the middle point 
         FP = f(p)
 
+        # checking if f(p) is the actual root or if the gap between the 
+        # extreme points is less than the tollerance.
+        # if so we return the p point and number of iterations 
         if (FP == 0 or ((b - a)/2) < TOL):
-            print(p)
             return p, i
         
         i += 1
+
+        # we multiply the sign of the left extreme evaluation with the midpoint
+        # evaluation and if the result is greater than 0 then we set a = p so 
+        # we moving the left extreme to the mid point 
         if ((np.sign(FA) * np.sign(FP)) > 0):
             a = p
             FA = FP
@@ -57,10 +72,11 @@ def BisectionAlgorithm(f, a, b, TOL, maxIters):
 
     print("couldn't evaluate the Bisection Theorem")
 
+# defining f(x)
 def f(x):
-        return x**(1/2) - math.cos(x)
+        return x**3-7*x**2+14*x-6
     
-root = BisectionAlgorithm(f, a=0, b=1, TOL=1e-10, maxIters=100)
+root = BisectionAlgorithm(f, a=0, b=1, TOL=1e-2, maxIters=100)
 print(root)    
 
 
